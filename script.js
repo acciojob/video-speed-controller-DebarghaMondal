6b56bef37fda5f0fd895bd711127725f19467a98
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-22 11:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.player__video');
+const progress = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const volume = document.querySelector('input[name="volume"]');
+const playbackRate = document.querySelector('input[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('.player__button[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progress.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+// Event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+volume.addEventListener('change', handleRangeUpdate);
+volume.addEventListener('mousemove', handleRangeUpdate);
+
+playbackRate.addEventListener('change', handleRangeUpdate);
+playbackRate.addEventListener('mousemove', handleRangeUpdate);
+
+progress.addEventListener('click', scrub);
